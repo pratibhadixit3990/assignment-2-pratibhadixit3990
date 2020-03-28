230e863ed0ad71e2b6f7195cae7aaf2068bd3e3a
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 18:40:58 +0530

    method printPostOrder to print the tree in post order completed

--- src/problem2/traversing/Traversal.java
+++ src/problem2/traversing/Traversal.java
@@ -1,8 +1,17 @@
 package problem2.traversing;
 
+import problem2.node.TreeNode;
+
 public class Traversal {
 
-    public void printPostorder() {
+    public void printPostorder(TreeNode node) {
+        if (node == null)
+            return;
 
+        printPostorder(node.getLeft());
+
+        printPostorder(node.getRight());
+
+        System.out.print(node.getData() + " ");
     }
 }

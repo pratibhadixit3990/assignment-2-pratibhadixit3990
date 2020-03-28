861a4780ec2fb9692c79869f94336da4dc2b93c9
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 20:33:28 +0530

    object of MyBinarySearchTree class declared in MyMain class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,10 +7,14 @@
 package problem4.main;
 // executable class
 
+import problem2.mybst.MyBinarySearchTree;
+
 import java.util.Scanner;
 
 public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        MyBinarySearchTree objectOfMyBinarySearchTree = new MyBinarySearchTree();
     }
 }

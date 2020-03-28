ecf1f0d1d2f9be29f65f4af462a0e55d54f8784f
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 21:16:18 +0530

    insertion and printing completed in MyMain class of problem5

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -7,6 +7,7 @@
 package problem5.main;
 
 import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
 
 import java.util.Scanner;
 
@@ -16,5 +17,28 @@ public class MyMain {
         Scanner scanner = new Scanner(System.in);
 
         MyCircularQueue objectOfMyCircularQueue = new MyCircularQueue();
+
+        System.out.println("Enter the size for circular queue: ");
+        int size = scanner.nextInt();
+
+        int array[] = new int[size];
+
+        int count = 0;
+        String studentName = "";
+        int backLogs;
+        for (int i = 0; i < size; i++) {
+            scanner.nextLine();
+            System.out.println("Enter the name of the Student: ");
+            studentName = scanner.nextLine();
+            System.out.println("Enter thew number of Backlogs of the Student: ");
+            backLogs = scanner.nextInt();
+
+            Student student = new Student(studentName, backLogs);
+            objectOfMyCircularQueue.enqueue(student);
+        }
+
+        System.out.println("Total number of students: " + objectOfMyCircularQueue.getSize());
+        System.out.println("Number of student/students having zero(0) backlogs: " + objectOfMyCircularQueue.remove());
+        System.out.println("Number of student/students having atleast ont(1) or more backlogs: " + (objectOfMyCircularQueue.getSize() - objectOfMyCircularQueue.remove()));
     }
 }

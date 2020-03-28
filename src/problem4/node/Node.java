30dcbc4f8c593e7ae206e429a02915a054f1e97d
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 20:13:33 +0530

    constructor of Node class declared

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -5,4 +5,9 @@ import problem2.node.TreeNode;
 public class Node {
     private Node next;
     private TreeNode node;
+
+    public Node(int data) {
+        node = new TreeNode(data);
+        next = null;
+    }
 }

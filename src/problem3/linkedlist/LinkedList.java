508ffc6de17f41bd24ab7af928aa450702806788
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 19:32:28 +0530

    method addLast() in the LinkedList class of problem3 completed

--- src/problem3/linkedlist/LinkedList.java
+++ src/problem3/linkedlist/LinkedList.java
@@ -1,12 +1,30 @@
 package problem3.linkedlist;
 
 import problem3.node.Node;
+import problem3.student.Student;
 
 public class LinkedList {
     private Node first;
     private Node last;
 
-    public void addLast() {
+    public void addLast(Student student) {
+        Node newNode = new Node(student);
+        if (first == null) {
+            first = last = newNode;
+            return;
+        }
 
+        last.setNext(newNode);
+        last = newNode;
+
+        var now = first;
+        while (now != null) {
+            if (newNode.getData().getRollNo() < now.getData().getRollNo()) {
+                var temp = newNode.getData();
+                newNode.setData(now.getData());
+                now.setData(temp);
+            }
+            now = now.getNext();
+        }
     }
 }

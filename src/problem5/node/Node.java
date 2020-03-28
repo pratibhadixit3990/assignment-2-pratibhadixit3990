4bd8445b2af4c9661d26d32d5178305344475da0
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 20:48:04 +0530

    constructor of Node class declared in problem5

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -12,4 +12,9 @@ import problem5.student.Student;
 public class Node {
     private Student data;
     private Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        next = null;
+    }
 }

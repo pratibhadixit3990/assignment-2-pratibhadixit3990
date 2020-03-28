11093b718eaacf0a6444529e7b157da8d0b5e16c
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-03-28 19:23:07 +0530

    constructor declared in Student class in problem3

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -3,4 +3,9 @@ package problem3.student;
 public class Student {
     private String name;
     private int rollNo;
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
 }
